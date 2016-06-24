631d2e1d3026d0808bd4c079f398bc73eaac07ff
Sahasha Reese <codeHer@icloud.com>
2016-06-23 21:51:55 -0500

    initial Commit

--- group_01.js
+++ group_01.js
@@ -4,3 +4,56 @@ var boo = ["Boo", "11435", "54000", 3];
 var scout = ["Scout", "6243", "74750", 5];
 
 var employees = [atticus, jem, boo, scout];
+//Step 1
+function addArray(empArray) {
+  var finalArray = [];
+  for(var i=0; i<empArray.length; i++)
+  finalArray.push (createEmployee(empArray[i]));
+  return finalArray;
+for (var i = 0; i < finalArray.length; i++) {
+  console.log(finalArray[i]);
+}
+}
+
+//Step 3
+function createEmployee(nameArray) {
+  newEmpArray = [];
+  newEmpArray[0] = nameArray[0];
+  newEmpArray[1] = bonus(nameArray);
+  newEmpArray[3] = Math.round(newEmpArray[1] * nameArray[2]);
+  newEmpArray[2] = newEmpArray[3] + parseInt(nameArray[2]);
+  newEmpArray[1] = newEmpArray[1] * 100 + '%';
+  return newEmpArray;
+}
+
+//Step 4
+
+function bonus (nameArray) {
+  var employeeBonus = 0;
+  if(nameArray[3] <= 2) {
+    employeeBonus += 0;
+  }
+    else if (nameArray[3] === 3) {
+      employeeBonus += .04;
+    }
+    else if (nameArray[3] === 4) {
+      employeeBonus += .06;
+    }
+    else if (nameArray[3] === 5) {
+      employeeBonus += .10;
+    }
+  if (nameArray[1].length === 4) {
+    employeeBonus += .05;
+  }
+  if (parseInt(nameArray[2]) > 65,000) {
+    employeeBonus -= .01;
+  }
+  if (employeeBonus > .13) {
+    employeeBonus = .13;
+}
+return employeeBonus
+}
+
+//Step 4
+
+addArray(employees);
